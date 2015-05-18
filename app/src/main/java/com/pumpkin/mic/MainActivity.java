d9a40b395f8145380301553043e32c1abd862944
pumpkinone <pumpkinbytez@gmail.com>
2015-05-18 23:16:48 +0900

    Added basic Mic,Stop, Play functions with clock widget.

--- app/src/main/java/com/pumpkin/mic/MainActivity.java
+++ app/src/main/java/com/pumpkin/mic/MainActivity.java
@@ -1,10 +1,16 @@
 package com.pumpkin.mic;
 
+import java.io.File;
+import java.io.IOException;
+
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
 import android.content.Context;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,17 +24,32 @@ public class MainActivity extends ActionBarActivity {
     private static final String TOAST_TEXT = "Test ads are being shown. "
             + "To show live ads, replace the ad unit ID in res/values/strings.xml with your own ad unit ID.";
 
-    private Button start,stop;
+    private Button Mic, Stop, Play;
+    private static String outputFile;
+    private MediaRecorder myAudioRecorder = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        start = (Button)findViewById(R.id.start_button);
-        stop = (Button)findViewById(R.id.stop_button);
+        Mic = (Button)findViewById(R.id.Mic_button);
+        Stop = (Button)findViewById(R.id.Stop_button);
+        Play = (Button)findViewById(R.id.button3);
 
-        // Load an ad into the AdMob banner view.
-        AdView adView = (AdView) findViewById(R.id.adView);
+        outputFile = Environment.getExternalStorageDirectory().
+                getAbsolutePath() + "/myrecording.3gp";
+
+        myAudioRecorder = new MediaRecorder();
+        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        myAudioRecorder.setOutputFile(outputFile);
+        Play.setEnabled(false);
+        Stop.setEnabled(false);
+
+                        // Load an ad into the AdMob banner view.
+                AdView adView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
 
@@ -58,14 +79,56 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void start(View view)  {
-        Toast.makeText(getApplicationContext(), "Mic is ready ", Toast.LENGTH_LONG).show();
+    public void Mic(View view)  {
+        if (!Stop.isEnabled()) {
+            Stop.setEnabled(true);
+        }
+        try {
+            myAudioRecorder.prepare();
+            myAudioRecorder.start();
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        if (Mic.isEnabled())
+        {
+            Mic.setEnabled(false);
+        }
 
+
+        Toast.makeText(getApplicationContext(), "Live Mic started", Toast.LENGTH_LONG).show();
     }
 
-    public void stop(View view)  {
-        Toast.makeText(getApplicationContext(), "Mic stopped", Toast.LENGTH_LONG).show();
+    public void Stop(View view) {
+        if (!Play.isEnabled()) {
+            Play.setEnabled(true);
+        }
+        if (!Mic.isEnabled()) {
+            Mic.setEnabled(true);
+        }
+        myAudioRecorder.stop();
+        myAudioRecorder.release();
+        myAudioRecorder = null;
+        Toast.makeText(getApplicationContext(), "Stopped", Toast.LENGTH_LONG).show();
+    }
 
+    public void Play(View view) throws IllegalArgumentException,
+            SecurityException, IllegalStateException, IOException{
+        if (Mic.isEnabled()) {
+            Mic.setEnabled(false);
+        }
+        if (!Stop.isEnabled()) {
+            Stop.setEnabled(true);
+        }
+        MediaPlayer m = new MediaPlayer();
+        m.setDataSource(outputFile);
+        m.prepare();
+        m.start();
+        Mic.setEnabled(true);
+         Toast.makeText(getApplicationContext(), "Playing audio", Toast.LENGTH_LONG).show();
     }
 
 
